59e5cac64fdf08f78615c8aa1c7d1f9d21dd70c2
Salah Saleh <salah.saleh.ali@gmail.com>
2026-02-23 21:46:09 +0200

    select mode from menubar

--- Frespr/MenuBar/MenuBarController.swift
+++ Frespr/MenuBar/MenuBarController.swift
@@ -1,6 +1,6 @@
 import AppKit
 
-final class MenuBarController {
+final class MenuBarController: NSObject, NSMenuDelegate {
     private var statusItem: NSStatusItem?
 
     var onSettings: (() -> Void)?
@@ -15,6 +15,13 @@ final class MenuBarController {
         button.image?.isTemplate = true
 
         let menu = NSMenu()
+        menu.delegate = self
+
+        let ppItem = NSMenuItem(title: "Post-processing", action: nil, keyEquivalent: "")
+        ppItem.submenu = buildPostProcessingMenu()
+        menu.addItem(ppItem)
+
+        menu.addItem(.separator())
 
         let settingsItem = NSMenuItem(title: "Settings…", action: #selector(openSettings), keyEquivalent: ",")
         settingsItem.target = self
@@ -29,6 +36,25 @@ final class MenuBarController {
         item.menu = menu
     }
 
+    func menuWillOpen(_ menu: NSMenu) {
+        if let ppItem = menu.item(withTitle: "Post-processing") {
+            ppItem.submenu = buildPostProcessingMenu()
+        }
+    }
+
+    private func buildPostProcessingMenu() -> NSMenu {
+        let sub = NSMenu()
+        let current = AppSettings.shared.postProcessingMode
+        for (i, mode) in PostProcessingMode.allCases.enumerated() {
+            let item = NSMenuItem(title: mode.displayName, action: #selector(setMode(_:)), keyEquivalent: "")
+            item.target = self
+            item.tag = i
+            item.state = mode == current ? .on : .off
+            sub.addItem(item)
+        }
+        return sub
+    }
+
     func setIcon(_ icon: MenuBarIcon) {
         let name: String
         switch icon {
@@ -38,6 +64,12 @@ final class MenuBarController {
         }
         statusItem?.button?.image = NSImage(systemSymbolName: name, accessibilityDescription: "Frespr")
         statusItem?.button?.image?.isTemplate = true
+    }
+
+    @objc private func setMode(_ sender: NSMenuItem) {
+        let modes = PostProcessingMode.allCases
+        guard sender.tag < modes.count else { return }
+        AppSettings.shared.postProcessingMode = modes[sender.tag]
     }
 
     @objc private func openSettings() { onSettings?() }
